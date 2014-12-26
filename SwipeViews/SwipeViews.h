cac5a7613f18f62b0f8dc111665153d6fa871245
tuvalusoftware <clark_obrien@yahoo.com>
2014-12-26 11:18:21 -0800

    Initial Commit

---
+++ SwipeViews/SwipeViews.h
@@ -0,0 +1,19 @@
+//
+//  SwipeViews.h
+//  SwipeViews
+//
+//  Created by TouchROI on 12/26/14.
+//  Copyright (c) 2014 tim.obrien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwipeViews.
+FOUNDATION_EXPORT double SwipeViewsVersionNumber;
+
+//! Project version string for SwipeViews.
+FOUNDATION_EXPORT const unsigned char SwipeViewsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwipeViews/PublicHeader.h>
+
+
